13a7019e1d334d16ec1d68f6768630b8022e7222
talmaw19 <talmaw19@my.whitworth.edu>
2016-10-23 16:42:25 -0700

    Update listcomp2.py

--- listcomp2.py
+++ listcomp2.py
@@ -2,15 +2,14 @@
 numbers = [1,2,3,4,5,6]
 oddNumbers = [x for x in nums for x % 2 == 1 ]
 if oddNumber > 5:
-   print oddNubers.lower() ''' shows the lower case of each string that is less than 5''''
+   print oddNumbers.lower() ''' shows the lower case of each string that is less than 5''''
 else:
-  print oddnumbers 
-  
+   print oddnumbers 
 oddNumsPlus1 =[x+1 for x in numbers if x % 2 = 1]
 if oddNumbers > 5: 
-  print oddNumbers.lower()
+   print oddNumbers.lower()
 else:
-  print oddNembers 
+   print oddNumbers 
   
 '''"part 2 "'''
 strings = ['Some string','Art','Music','Artificial Intelligence']
